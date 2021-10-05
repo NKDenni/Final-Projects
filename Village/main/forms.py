5656993a6a6fd6d15d84cfe068ae1c8173490868
Nathan Denni <aubakasan303@gmail.com>
2021-10-05 17:12:09 +0900

    Second try of final solo project

--- Village/main/forms.py
+++ Village/main/forms.py
@@ -8,3 +8,11 @@ class UserForm(forms.Form):
     email = forms.CharField(max_length=200)
     birthday = forms.DateField(auto_now=False)
     password = forms.CharField(max_length=16)
+
+class ChildForm(forms.Form):
+    #id
+    name = forms.CharField(max_length=200)
+    birthday = forms.DateField(auto_now=False)
+    gender = forms.CharField(max_length=200)
+    econtact = forms.CharField(max_length=16)
+    enumber = forms.CharField(max_length=16)
