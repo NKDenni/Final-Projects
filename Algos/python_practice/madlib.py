c0c51c263fabdf8d5f9364d09b7e9516ecce7462
NKDenni <nathandenni@gmail.com>
2023-09-16 12:50:08 +0900

    test script and python practice

---
+++ Algos/python_practice/madlib.py
@@ -0,0 +1,27 @@
+'''
+Mad_Lib generator
+'''
+
+# User Inputs
+
+noun = input(' Pick a noun: ')
+print(noun)
+
+noun2 = input(' Pick a plural noun: ')
+print(noun2)
+
+adjective = input(' Pick an adjective: ')
+print(adjective)
+
+
+
+
+
+# Mad Lib template
+
+print(f"This weekend I am going camping with {noun}. I packed my lantern, sleeping bag, and ________________.\nI am so ________________ to ________________ in a tent. I am ________________ we")
+might see a ________________, they are kind of dangerous. We are going to hike, fish, and ________________.
+I have heard that the ________________ lake is great for ________________. Then we will
+________________hike through the forest for ________________ ________________. If I see a
+________________ ________________ while hiking, I am going to bring it home as a pet! At night we will tell
+________________ _______________ stories and roast ________________ around the campfire!!')
